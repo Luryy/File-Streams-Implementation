93f00ee9a3ff81d17fc3a2904ba0d02e36cb13b5
Lucas Yuri <lucasyuri56@hotmail.com>
2021-03-16 17:30:29 -0300

    feat: create nodejs file streamstream

--- src/controller/StreamController.ts
+++ src/controller/StreamController.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from 'express';
 
-class BasicController {
-  async get(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json({ streamRoute: 'ok' });
+import fs from 'fs';
+import { resolve } from 'path';
+
+class StreamController {
+  async get(req: Request, res: Response): Promise<void> {
+    const { range } = req.headers;
+    const filePath = resolve(__dirname, '..', 'upload', 'ex.mp4');
+    const { size: fileSize } = fs.statSync(filePath);
+
+    const start = Number(range?.replace(/\D/g, '')) || 0;
+    const CHUNK_SIZE = 1000000;
+    const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
+
+    const headers = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, headers);
+
+    const fileStreams = fs.createReadStream(filePath, { start, end });
+
+    fileStreams.pipe(res);
   }
 }
 
-export default BasicController;
+export default StreamController;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { resolve } from 'path';
 
 import StreamController from '../controller/StreamController';
 
@@ -7,6 +8,10 @@ const routes = Router();
 const streamController = new StreamController();
 
 routes.get('/check', (req, res) => res.json({ status: 'ok' }));
+routes.get('/html-stream', (req, res) =>
+  res.sendFile(resolve(__dirname, '..', 'html', 'index.html')),
+);
+
 routes.get('/stream', streamController.get);
 
 export default routes;
