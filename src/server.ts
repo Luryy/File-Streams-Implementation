0e2632e589ac2fb2fc970511d5a12a859e55b664
Lucas Yuri <lucasyuri56@hotmail.com>
2021-03-17 14:43:20 -0300

    feat: add memory log control

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import app from './app';
 
+setInterval(() => {
+  const processUsage = process.memoryUsage();
+
+  const totalUsedMemoryInMb = Math.floor(processUsage.rss / (1024 * 1024));
+
+  console.log(`Used Memory: ${totalUsedMemoryInMb} MB`);
+}, 5 * 1000);
+
 const port = process.env.APP_PORT || 3333;
 
 app.listen(port, () => {
