822667a1fcb9e1f50f6f1d1e2b4cee19b77d2334
Lucas Yuri <lucasyuri56@hotmail.com>
2021-03-17 14:25:56 -0300

    feat: create individual pages to each method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,6 +15,15 @@ routes.get('/check', (req, res) => res.json({ status: 'ok' }));
 routes.get('/html-all', (req, res) =>
   res.sendFile(resolve(__dirname, '..', 'html', 'index.html')),
 );
+routes.get('/html-stream', (req, res) =>
+  res.sendFile(resolve(__dirname, '..', 'html', 'index-stream.html')),
+);
+routes.get('/html-send-file', (req, res) =>
+  res.sendFile(resolve(__dirname, '..', 'html', 'index-send-file.html')),
+);
+routes.get('/html-send-sync', (req, res) =>
+  res.sendFile(resolve(__dirname, '..', 'html', 'index-send-sync.html')),
+);
 
 routes.get('/stream', streamController.get);
 routes.get('/send-file', sendFileController.get);
